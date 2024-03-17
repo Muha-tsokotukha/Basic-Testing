e9d680272bf8fa35732352042109db9fefe17716
Mukhit Home <vinniedrew7@gmail.com>
2024-03-17 18:00:15 +0500

    ref: aaa

--- src/math.test.js
+++ src/math.test.js
@@ -2,7 +2,13 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
+  // Arrange
+  const numbers = [1, 2, 3];
+  const expectedResult = numbers.reduce((acc, curr) => acc + curr, 0);
 
-  expect(result).toBe(6);
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(expectedResult);
 });
